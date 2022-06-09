57a8d0d29f107718f937b7450dabaa283f45e243
wasabi-suki <107193340+wasabi-suki@users.noreply.github.com>
2022-06-09 22:51:55 +0900

    const,let等の変数宣言

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@
 // console.log(val4);
 
 //constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
