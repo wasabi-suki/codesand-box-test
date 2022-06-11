0070174e3f6b998d4f53dd0f079b25870aca513c
wasabi-suki <107193340+wasabi-suki@users.noreply.github.com>
2022-06-11 11:14:41 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -44,3 +44,18 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "杉山";
+// const age = 20;
+// //「私の名前は杉山です。年齢は20歳です。」
+
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
